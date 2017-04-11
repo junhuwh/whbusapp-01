253868b9bb32827a64cf4bcb22fd9870eff59bdf
junhuwh <junhuwh@cn.ibm.com>
2017-04-11 10:35:42 +0800

    2

--- src/main/java/com/ibm/wuhan/TestServlet.java
+++ src/main/java/com/ibm/wuhan/TestServlet.java
@@ -1,6 +1,8 @@
 package com.ibm.wuhan;
 
 import java.io.IOException;
+import java.util.Random;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,51 +10,57 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TestServlet
- */
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+	public TestServlet() {
+
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		test3(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().append("doget: ").append(request.getContextPath());
+	// 浏览器定时刷新
+
+	private void test(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		response.setHeader("refresh", "3");
+
+		String data = new Random().nextInt(10000) + "";
+		response.getWriter().write(data);
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	// 浏览器自动跳转
+	private void test2(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		response.setHeader("refresh", "3;url='index.html'");
+		response.getWriter().write("没有跳转请点击 <a href='index.html'>跳转</a>");
+
+	}
+
+	// 实用浏览器自动跳转
+	private void test3(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		String message ="<meta http-equiv='refresh' content='5;url=index.html'>消息 from servlet111";
+		this.getServletContext().setAttribute("message1", message);
+		this.getServletContext().getRequestDispatcher("/message.jsp").forward(request, response);
+		
+
+
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		doGet(request, response);
-		response.getWriter().append("<br>dopost: ").append(request.getContextPath());
+
 	}
 
 }
